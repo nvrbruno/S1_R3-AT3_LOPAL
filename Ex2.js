00faa2931d884dcbdad15c9b7f4b97256e0ef7e1
unknown <brunonavarrodavi@gmail.com>
2025-04-22 13:59:05 -0300

    Exercício 2

--- Ex2.js
+++ Ex2.js
@@ -2,9 +2,7 @@ let soma = 0;
 
 for (let i = 1; i <= 10; i++) { // Loop que repete 10 vezes
     let num = parseInt(prompt(`Digite o ${i}º número inteiro:`)); // Pede o valor de acordo com o I até 10
-
     if (isNaN(num)) { // Verifica se o valor digitado não é um número
-        i--; // Volta uma posição no loop para tentar de novo
     } else {
         soma += num; // Soma o número digitado à variável 'soma'
     }
