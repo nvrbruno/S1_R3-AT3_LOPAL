de91d67e370071789ff8a9e2a2e1af847422ab31
unknown <brunonavarrodavi@gmail.com>
2025-04-22 12:35:31 -0300

    Exercicío 1

--- Ex1.js
+++ Ex1.js
@@ -1,9 +1,14 @@
-const num = parseInt(prompt("Digite um número para mostrar quantos números do 1 ao número informado:"));
+// Solicita ao usuário que digite um número e converte a entrada para um número inteiro
+const num = parseInt(prompt("Digite um número para mostrar quantos números do 1 ao número informado:"));  
 
-if (isNaN(num)){
-    alert("Erro! Digite um número válido")
-}else{
-    for (let i = 1; i <= num; i++){
-        alert(`${i}`)
-    }
+// Verifica se o valor digitado não é um número
+if (isNaN(num)) {     
+    // Exibe uma mensagem de erro se o valor não for um número
+    alert("Erro! Digite um número válido") 
+} else {     
+    // Se o número for válido, inicia um loop que vai de 1 até o número digitado pelo usuario
+    for (let i = 1; i <= num; i++) {         
+        // Exibe um alerta com o número atual do loop
+        alert(`${i}`)     
+    } 
 }
